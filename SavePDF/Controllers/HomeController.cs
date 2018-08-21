241464eb4be44b644bae0670383492300149aa1d
rchowdary <roopeshchowdary@live.com>
2018-08-21 00:35:58 -0500

    first commit

---
+++ SavePDF/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa.MVC;
+
+namespace SavePDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewToPdf()
+        {
+            // This will directly download the view as pdf file
+            //return new ActionAsPdf( "Index")
+            //                        { FileName = "ViewAsPdf.pdf" };
+
+            // This will open the view as pdf file and user can then print or save
+            var pdfResult = new ViewAsPdf("Index")
+            { FileName = "ViewAsPdf.pdf" };
+
+            var binary = pdfResult.BuildPdf(this.ControllerContext);
+            return File(binary, "application/pdf");
+        }
+    }
+}
